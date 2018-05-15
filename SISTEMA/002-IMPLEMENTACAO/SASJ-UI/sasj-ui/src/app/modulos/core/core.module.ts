cd517bf07e5ad966e5a5fe1e4823d7e15b51f8cb
Vinícius Gomes <vinicius.gouveia.gomes@gmail.com>
2018-05-15 16:29:53 -0300

    Modificações referentes à adição de novos módulos; criação de páginas para urls desconhecidas ou para acesso negado; classe modelo para usuário; modificações na classe de serviço errorHandler

--- SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/modulos/core/core.module.ts
+++ SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/modulos/core/core.module.ts
@@ -1,15 +1,18 @@
-import { ErrorHandlerService } from './error-handler.service';
-import { AuthService } from './../seguranca/auth.service';
-
-import { JwtHelper } from 'angular2-jwt';
-
-import { NavbarModule } from './../navbar/navbar.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { RouterModule } from '@angular/router';
 
+import { AuthService } from './../seguranca/auth.service';
+import { LogoutService } from './../seguranca/logout.service';
+import { UsuarioService } from './../usuario/usuario.service';
+import { NaoAutorizadoComponent } from './nao-autorizado.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada.component';
+import { ErrorHandlerService } from './error-handler.service';
+import { NavbarModule } from './../navbar/navbar.module';
+
+import { JwtHelper } from 'angular2-jwt';
 
 @NgModule({
   imports: [
@@ -18,15 +21,17 @@ import { RouterModule } from '@angular/router';
     NavbarModule,
     RouterModule
   ],
-  declarations: [],
+  declarations: [PaginaNaoEncontradaComponent, NaoAutorizadoComponent],
   exports: [
     NavbarModule,
     MatSnackBarModule
   ],
   providers: [
     AuthService,
+    LogoutService,
     JwtHelper,
-    ErrorHandlerService
+    ErrorHandlerService,
+    UsuarioService
   ]
 
 })
---
+++ SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/modulos/core/model.ts
@@ -0,0 +1,12 @@
+export class Usuario {
+
+  codigo: number;
+  nome: string;
+  cargo: string;
+  matricula: string;
+  senha: string;
+  email: string;
+  tipoUsuario: string;
+  ativo: boolean;
+
+}
