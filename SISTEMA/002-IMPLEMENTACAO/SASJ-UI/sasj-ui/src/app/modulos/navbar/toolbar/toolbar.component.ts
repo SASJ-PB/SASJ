bd7ea390f37548fe3dc40d7c3f3f5b127202b058
Vinícius Gomes <vinicius.gouveia.gomes@gmail.com>
2018-05-15 16:31:18 -0300

    Modificação no menu de usuário logado

--- SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/modulos/navbar/toolbar/toolbar.component.ts
+++ SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/modulos/navbar/toolbar/toolbar.component.ts
@@ -1,6 +1,12 @@
-import { MediaMatcher } from '@angular/cdk/layout';
 import { Component, OnInit, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+
 import { MatSidenav } from '@angular/material';
+
+import { LogoutService } from './../../seguranca/logout.service';
+import { AuthService } from './../../seguranca/auth.service';
+import { ErrorHandlerService } from './../../core/error-handler.service';
 
 @Component({
   selector: 'app-toolbar',
@@ -21,23 +27,44 @@ export class ToolbarComponent implements OnInit {
     {label: 'Relatórios', icone: 'description'}
   ];
 
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher,
+      private authService: AuthService, private logoutService: LogoutService,
+      private errorHandlerService: ErrorHandlerService, private router: Router) {
 
     // se a largura da tela for 700 ou mais, o menu lateral fica fixo e
     // aparece ao lado do conteúdo se não, ele é ocultado e sobrepõe o conteúdo.
-    this.mobileQuery = media.matchMedia('(max-width: 700px)');
 
-    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileQuery.addListener(this._mobileQueryListener);
+    if (this.router.url !== '/login'){
+
+      this.mobileQuery = media.matchMedia('(max-width: 700px)');
+
+      this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+      this.mobileQuery.addListener(this._mobileQueryListener);
+    }
   }
 
   ngOnInit() {
 
-    if (this.mobileQuery.matches) {
-      this.sidenavRef.close();
+    if (this.router.url !== '/login'){
+      if (this.mobileQuery.matches) {
+        this.sidenavRef.close();
+      }
+      else {
+        this.sidenavRef.open();
+      }
     }
-    else {
-      this.sidenavRef.open();
-    }
+
+  }
+
+  ocultarToolbar() {
+    return this.router.url !== '/login';
+  }
+
+  logout() {
+    this.logoutService.logout()
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch(erro => this.errorHandlerService.handle(erro));
   }
 }
