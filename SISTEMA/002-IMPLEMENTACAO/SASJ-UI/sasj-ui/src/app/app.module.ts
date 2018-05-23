be80cb60bbba7aaa18ba1d2f9118dfaf23b14e89
Vinícius Gomes <vinicius.gouveia.gomes@gmail.com>
2018-05-23 20:39:43 -0300

    Adição da dependência necessária para utilização do componente 'slide togle'

--- SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/app.module.ts
+++ SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
