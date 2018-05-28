f40c20cb7d4951009f6cdfb6df17fd0cd48b3e9d
Vinícius Gomes <vinicius.gouveia.gomes@gmail.com>
2018-05-28 15:05:11 -0300

    Atualização dos dados da tabela ao remover um agendamento

--- SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/modulos/agendamento/agendamento-remocao/agendamento-remocao.component.ts
+++ SISTEMA/002-IMPLEMENTACAO/SASJ-UI/sasj-ui/src/app/modulos/agendamento/agendamento-remocao/agendamento-remocao.component.ts
@@ -1,4 +1,3 @@
-import { MatPaginator } from '@angular/material/paginator';
 import { Conciliacao, Audiencia } from './../../core/model';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -8,6 +7,7 @@ import { MatDialog, MAT_DIALOG_DATA, MatTableDataSource } from '@angular/materia
 import { Component, OnInit, Input, Inject, TemplateRef } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Location } from '@angular/common';
+import { DataSource } from '@angular/cdk/table';
 
 @Component({
   selector: 'app-agendamento-remocao',
@@ -18,7 +18,7 @@ export class AgendamentoRemocaoComponent implements OnInit {
 
   @Input() tipoSessao: string;
   @Input() idSessao: number;
-  @Input() paginator: TemplateRef<MatPaginator>;
+  @Input() tabelaRef: TemplateRef<MatTableDataSource<any>>;
 
   constructor(public dialog: MatDialog) { }
 
@@ -26,14 +26,12 @@ export class AgendamentoRemocaoComponent implements OnInit {
 
   openDialog() {
 
-    //console.log(this.paginator);
-
     const dialogRef = this.dialog.open(AgendamentoRemocaoDialogComponent, {
       height: '45%',
       data: {
           tipoSessao: this.tipoSessao,
           idSessao: this.idSessao,
-          paginatorRef: this.paginator
+          tabelaRef: this.tabelaRef
         }
     });
   }
@@ -46,29 +44,37 @@ export class AgendamentoRemocaoComponent implements OnInit {
 })
 export class AgendamentoRemocaoDialogComponent implements OnInit {
 
-  paginatorRef: MatPaginator;
+  tabelaRef: MatTableDataSource<any>;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private location: Location,
       private agendamentoService: AgendamentoService, private router: Router,
       private errorHandlerService: ErrorHandlerService, private snackBar: MatSnackBar) {}
 
   ngOnInit() {
-    console.log(this.data.paginator);
+
   }
 
   excluir() {
 
-    this.paginatorRef = this.data.paginatorRef;
+    this.tabelaRef = this.data.tabelaRef;
 
     if (this.data.tipoSessao === 'audiencia'){
+
       this.agendamentoService.excluirAudiencia(this.data.idSessao)
         .then(() => {
-          // console.log(this.tabelaRef['dataSource']);
-          // this.agendamentoService.listarAudiencias();
-          //this.paginatorRef.pageIndex = 0;
-          window.location.reload();
 
-          //this.router.navigate(['/agendamentos']);
+          // Atribuição dos dados em um array para utilizar o método 'filter'
+          const dadosAudiencias: Audiencia[] = this.tabelaRef['dataSource'].data;
+
+          // Filtragem do elemento que será removido da tabela
+          const audienciaFiltrada = dadosAudiencias.filter(resultado =>
+              resultado.codigo === this.data.idSessao)[0];
+
+          // Remoção do elemento
+          dadosAudiencias.splice(dadosAudiencias.indexOf(audienciaFiltrada), 1);
+
+          // Atualização dos dados
+          this.tabelaRef['dataSource'].data = dadosAudiencias;
 
           this.snackBar.open('Audiência excluída com sucesso', '', { duration: 4500});
         })
@@ -77,19 +83,30 @@ export class AgendamentoRemocaoDialogComponent implements OnInit {
         });
     }
     else {
+
       this.agendamentoService.excluirConciliacao(this.data.idSessao)
         .then(() => {
-          // console.log(this.tabelaRef['dataSource']);
-          window.location.reload();
-          //this.router.navigate(['/agendamentos']);
-          // this.agendamentoService.listarConciliacoes();
-          // this.tabelaConciliacaoRef.paginator.nextPage();
-          // this.tabelaConciliacaoRef.paginator.previousPage();
+
+          // Atribuição dos dados em um array para utilizar o método 'filter'
+          const dadosConciliacoes: Conciliacao[] = this.tabelaRef['dataSource'].data;
+
+          // Filtragem do elemento que será removido da tabela
+          const conciliacaoFiltrada = dadosConciliacoes.filter(resultado =>
+              resultado.codigo === this.data.idSessao)[0];
+
+          // Remoção do elemento
+          dadosConciliacoes.splice(dadosConciliacoes.indexOf(conciliacaoFiltrada), 1);
+
+          // Atualização dos dados
+          this.tabelaRef['dataSource'].data = dadosConciliacoes;
+
           this.snackBar.open('Conciliação excluída com sucesso', '', { duration: 4500});
         })
         .catch(erro => {
           this.errorHandlerService.handle(erro);
         });
     }
+
+    this.tabelaRef['dataSource']._updateChangeSubscription();
   }
 }
