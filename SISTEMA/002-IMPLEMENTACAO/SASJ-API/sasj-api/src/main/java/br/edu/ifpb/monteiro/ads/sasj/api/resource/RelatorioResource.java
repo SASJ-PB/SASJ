592866de5b19484ae8e01745730621c0119458fe
André Luís <tr.andreluis@gmail.com>
2018-07-17 16:38:49 -0300

    Implementação total da geração do Relatório de Quantidade de Oitiva Por Tipo de Audiência

--- SISTEMA/002-IMPLEMENTACAO/SASJ-API/sasj-api/src/main/java/br/edu/ifpb/monteiro/ads/sasj/api/resource/RelatorioResource.java
+++ SISTEMA/002-IMPLEMENTACAO/SASJ-API/sasj-api/src/main/java/br/edu/ifpb/monteiro/ads/sasj/api/resource/RelatorioResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.ifpb.monteiro.ads.sasj.api.model.relatorio.RelatorioQuantidadeOitivaTipoAudiencia;
 import br.edu.ifpb.monteiro.ads.sasj.api.model.relatorio.RelatorioQuantidadeTipoAudiencia;
 import br.edu.ifpb.monteiro.ads.sasj.api.service.RelatorioService;
 
@@ -21,12 +22,23 @@ public class RelatorioResource {
 	private RelatorioService relatorioService;
 
 	@GetMapping("/quantidadeTipoAudiencia")
-	public ResponseEntity<RelatorioQuantidadeTipoAudiencia> gerarRelatorio(
+	public ResponseEntity<RelatorioQuantidadeTipoAudiencia> gerarRelatorioQTA(
 			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime de,
 			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime ate) {
 
-		return relatorioService.gerarRelatorioQtdTipoAudiencia(de, ate);
+		RelatorioQuantidadeTipoAudiencia relatorioQTA = relatorioService.gerarRelatorioQtdTipoAudiencia(de, ate);
 
+		return ResponseEntity.ok().body(relatorioQTA);
 	}
 
+	@GetMapping("/quantidadeOitivaTipoAudiencia")
+	public ResponseEntity<RelatorioQuantidadeOitivaTipoAudiencia> gerarRelatorioQOTA(
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime de,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime ate) {
+
+		RelatorioQuantidadeOitivaTipoAudiencia relatorioQOTA = relatorioService.gerarRelatorioQtdOitivaTipoAudiencia(de, ate);
+
+		return ResponseEntity.ok().body(relatorioQOTA);
+	}
+	
 }
--- SISTEMA/002-IMPLEMENTACAO/SASJ-API/sasj-api/src/main/java/br/edu/ifpb/monteiro/ads/sasj/api/service/RelatorioService.java
+++ SISTEMA/002-IMPLEMENTACAO/SASJ-API/sasj-api/src/main/java/br/edu/ifpb/monteiro/ads/sasj/api/service/RelatorioService.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.edu.ifpb.monteiro.ads.sasj.api.enums.TipoAudiencia;
 import br.edu.ifpb.monteiro.ads.sasj.api.model.Audiencia;
+import br.edu.ifpb.monteiro.ads.sasj.api.model.relatorio.RelatorioQuantidadeOitivaTipoAudiencia;
 import br.edu.ifpb.monteiro.ads.sasj.api.model.relatorio.RelatorioQuantidadeTipoAudiencia;
 import br.edu.ifpb.monteiro.ads.sasj.api.repository.AudienciaRepository;
 import br.edu.ifpb.monteiro.ads.sasj.api.repository.ConciliacaoRepository;
@@ -23,24 +23,37 @@ public class RelatorioService {
 	@Autowired
 	private ConciliacaoRepository conciliacaoRepository;
 
-	public ResponseEntity<RelatorioQuantidadeTipoAudiencia> gerarRelatorioQtdTipoAudiencia(LocalDateTime de, 
-			LocalDateTime ate) {
+	public RelatorioQuantidadeTipoAudiencia gerarRelatorioQtdTipoAudiencia(LocalDateTime de, LocalDateTime ate) {
 		conciliacaoRepository.toString();
-		
+
 		AudienciaFilter filterData = new AudienciaFilter();
 		filterData.setDataAgendamentoDe(de);
 		filterData.setDataAgendamentoAte(ate);
-		
+
 		List<Audiencia> audiencias = audienciaRepository.filtrarPorData(filterData);
-		
+
 		RelatorioQuantidadeTipoAudiencia relatorioQTA = montarRelatorioQTA(audiencias);
 
-		return ResponseEntity.ok().body(relatorioQTA);
+		return relatorioQTA;
+
+	}
+
+	public RelatorioQuantidadeOitivaTipoAudiencia gerarRelatorioQtdOitivaTipoAudiencia(LocalDateTime de, LocalDateTime ate) {
+
+		AudienciaFilter filterData = new AudienciaFilter();
+		filterData.setDataAgendamentoDe(de);
+		filterData.setDataAgendamentoAte(ate);
+
+		List<Audiencia> audiencias = audienciaRepository.filtrarPorData(filterData);
+
+		RelatorioQuantidadeOitivaTipoAudiencia relatorioQOTA = montarRelatorioQOTA(audiencias);
+
+		return relatorioQOTA;
 
 	}
 
 	private RelatorioQuantidadeTipoAudiencia montarRelatorioQTA(List<Audiencia> audiencias) {
-		
+
 		int qtdPenal = 0;
 		int qtdAcaoCivil = 0;
 		int qtdCustodia = 0;
@@ -51,37 +64,80 @@ public class RelatorioService {
 		int qtdTebasImprobidade = 0;
 		int qtdVideoconferencia = 0;
 		int qtdOutros = 0;
-		
+
 		for (Audiencia audiencia : audiencias) {
-			if(audiencia.getTipoAudiencia() == TipoAudiencia.PENAL) {
+			if (audiencia.getTipoAudiencia() == TipoAudiencia.PENAL) {
 				qtdPenal++;
-			} else if(audiencia.getTipoAudiencia() == TipoAudiencia.ACAO_CIVIL) {
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.ACAO_CIVIL) {
 				qtdAcaoCivil++;
-			} else if(audiencia.getTipoAudiencia() == TipoAudiencia.CUSTODIA) {
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.CUSTODIA) {
 				qtdCustodia++;
-			} else if(audiencia.getTipoAudiencia() == TipoAudiencia.IMPROBIDADE) {
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.IMPROBIDADE) {
 				qtdImprobidade++;
-			} else if(audiencia.getTipoAudiencia() == TipoAudiencia.INSTRUCAO_CRETA) {
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.INSTRUCAO_CRETA) {
 				qtdInstrucaoCreta++;
-			} else if(audiencia.getTipoAudiencia() == TipoAudiencia.LEILAO) {
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.LEILAO) {
 				qtdLeilao++;
-			} else if(audiencia.getTipoAudiencia() == TipoAudiencia.PJE) {
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.PJE) {
 				qtdPJE++;
-			} else if(audiencia.getTipoAudiencia() == TipoAudiencia.TEBAS_IMPROBIDADE) {
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.TEBAS_IMPROBIDADE) {
 				qtdTebasImprobidade++;
-			} else if(audiencia.getTipoAudiencia() == TipoAudiencia.VIDEOCONFERENCIA) {
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.VIDEOCONFERENCIA) {
 				qtdVideoconferencia++;
 			} else {
 				qtdOutros++;
-			} 
+			}
 		}
-		
-		RelatorioQuantidadeTipoAudiencia RQTA = 
-				new RelatorioQuantidadeTipoAudiencia(qtdPenal, qtdAcaoCivil, qtdCustodia,
-						qtdImprobidade, qtdInstrucaoCreta, qtdLeilao, qtdPJE, qtdTebasImprobidade,
-						qtdVideoconferencia, qtdOutros);
-		
+
+		RelatorioQuantidadeTipoAudiencia RQTA = new RelatorioQuantidadeTipoAudiencia(qtdPenal, qtdAcaoCivil,
+				qtdCustodia, qtdImprobidade, qtdInstrucaoCreta, qtdLeilao, qtdPJE, qtdTebasImprobidade,
+				qtdVideoconferencia, qtdOutros);
+
 		return RQTA;
+	}
+
+	private RelatorioQuantidadeOitivaTipoAudiencia montarRelatorioQOTA(List<Audiencia> audiencias) {
+
+		int qtdOitivaPenal = 0;
+		int qtdOitivaAcaoCivil = 0;
+		int qtdOitivaCustodia = 0;
+		int qtdOitivaImprobidade = 0;
+		int qtdOitivaInstrucaoCreta = 0;
+		int qtdOitivaLeilao = 0;
+		int qtdOitivaPJE = 0;
+		int qtdOitivaTebasImprobidade = 0;
+		int qtdOitivaVideoconferencia = 0;
+		int qtdOitivaOutros = 0;
+
+		for (Audiencia audiencia : audiencias) {
+			if (audiencia.getTipoAudiencia() == TipoAudiencia.PENAL) {
+				qtdOitivaPenal += audiencia.getQuantidadeOitivas();
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.ACAO_CIVIL) {
+				qtdOitivaAcaoCivil += audiencia.getQuantidadeOitivas();
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.CUSTODIA) {
+				qtdOitivaCustodia += audiencia.getQuantidadeOitivas();
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.IMPROBIDADE) {
+				qtdOitivaImprobidade += audiencia.getQuantidadeOitivas();
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.INSTRUCAO_CRETA) {
+				qtdOitivaInstrucaoCreta += audiencia.getQuantidadeOitivas();
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.LEILAO) {
+				qtdOitivaLeilao += audiencia.getQuantidadeOitivas();
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.PJE) {
+				qtdOitivaPJE += audiencia.getQuantidadeOitivas();
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.TEBAS_IMPROBIDADE) {
+				qtdOitivaTebasImprobidade += audiencia.getQuantidadeOitivas();
+			} else if (audiencia.getTipoAudiencia() == TipoAudiencia.VIDEOCONFERENCIA) {
+				qtdOitivaVideoconferencia += audiencia.getQuantidadeOitivas();
+			} else {
+				qtdOitivaOutros += audiencia.getQuantidadeOitivas();
+			}
+		}
+
+		RelatorioQuantidadeOitivaTipoAudiencia RQOTA = new RelatorioQuantidadeOitivaTipoAudiencia(qtdOitivaPenal,
+				qtdOitivaAcaoCivil, qtdOitivaCustodia, qtdOitivaImprobidade, qtdOitivaInstrucaoCreta, qtdOitivaLeilao,
+				qtdOitivaPJE, qtdOitivaTebasImprobidade, qtdOitivaVideoconferencia, qtdOitivaOutros);
+
+		return RQOTA;
 	}
 
 }
